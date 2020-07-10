bc35df268cd7869edfaf46e6ad6be1f3e77ad614
Léo <66998902+Baheza-Lionel@users.noreply.github.com>
2020-07-10 13:16:02 +0200

    update

--- Exercices/exercice-quote-day/script.js
+++ Exercices/exercice-quote-day/script.js
@@ -1,1 +1,24 @@
 /* C'est ici qu'il faut rédiger ton code pour utiliser l'API */
+const getrandomQuotes = () => {
+  let randomNumber = Math.floor(Math.random() * Math.floor(1643));
+  fetch('https://type.fit/api/quotes')
+    .then(response => {
+      return response.json();
+    })
+    .then(response => {
+      let data = response[randomNumber];
+      console.log(data);
+      document.getElementById('quote').textContent = "\"" + data.text + "\"";
+      if (data.author === null || data.author === undefined) {
+        document.getElementById('author').textContent = 'unkown author';
+      } else {
+        document.getElementById('author').textContent = data.author;
+      }
+    });
+};
+/* getrandomQuotes();
+ */
+
+document.getElementById('alert').addEventListener('click', () => {
+  getrandomQuotes();
+});
